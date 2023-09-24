c011079d8df1ca59dd5ae6354883adcb2c3c67c3
jorge <jorge.andre.silva.fernandes@gmail.com>
2023-09-24 18:36:22 +0100

    Add delete product with active/non active.

--- src/main/java/com/example/crud/controllers/ProductController.java
+++ src/main/java/com/example/crud/controllers/ProductController.java
@@ -4,6 +4,7 @@ import com.example.crud.domain.product.Product;
 import com.example.crud.domain.product.ProductRepository;
 import com.example.crud.domain.product.RequestProduct;
 import com.example.crud.domain.product.RequestProductId;
+import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -22,7 +23,7 @@ public class ProductController {
 
     @GetMapping
     public ResponseEntity getAllProducts(){
-        var allProducts = repository.findAll();
+        var allProducts = repository.findAllByActiveTrue();
         return ResponseEntity.ok(allProducts);
 
     }
@@ -36,13 +37,14 @@ public class ProductController {
     }
 
     @PutMapping
+    @Transactional
     public ResponseEntity updateProduct(@RequestBody @Valid RequestProduct data) {
         Optional<Product> product = repository.findById(data.id());
         if (product.isPresent()) {
             Product newProduct = product.get();
             newProduct.setName(data.name());
             newProduct.setPrice_in_cents(data.price_in_cents());
-            repository.save(newProduct);
+//            repository.save(newProduct);
             return ResponseEntity.ok(newProduct);
         } else {
             return ResponseEntity.notFound().build();
@@ -58,14 +60,33 @@ public class ProductController {
 //        return ResponseEntity.ok(product);
 //    }
 
-    @DeleteMapping
-    public ResponseEntity<Void> deleteProduct(@RequestBody @Valid RequestProductId data) {
-        Optional<Product> product = repository.findById(data.id());
+//    @DeleteMapping
+//    public ResponseEntity<Void> deleteProduct(@RequestBody @Valid RequestProductId data) {
+//        Optional<Product> product = repository.findById(data.id());
+//        if (product.isPresent()) {
+//            repository.deleteById(data.id());
+//            return ResponseEntity.noContent().build();
+//        } else {
+//            return ResponseEntity.notFound().build();
+//        }
+//    }
+
+//    @DeleteMapping("/{id}")
+//    public ResponseEntity deleteProduct(@PathVariable String id) {
+//        repository.deleteById(id);
+//        return ResponseEntity.noContent().build();
+//    }
+    @DeleteMapping("/{id}")
+    @Transactional
+    public ResponseEntity deleteProduct(@PathVariable String id) {
+        Optional<Product> product = repository.findById(id);
         if (product.isPresent()) {
-            repository.deleteById(data.id());
+            Product newProduct = product.get();
+            newProduct.setActive(false);
             return ResponseEntity.noContent().build();
         } else {
             return ResponseEntity.notFound().build();
         }
     }
+
 }
